3563af5c63c562dae3f1e76a347c623780f36736
Lenildo <lourencolenildo@gmail.com>
2023-09-28 21:50:37 -0300

    Serviços e interceptador

--- FRONTEND-AUTO-PECAS/src/app/shared/global-constants.ts
+++ FRONTEND-AUTO-PECAS/src/app/shared/global-constants.ts
@@ -2,6 +2,8 @@ export class GlobalConstants {
     // Mensagem
     public static genericError: string = "Algo deu errado. Por favor, tente novamente mais tarde";
 
+    public static unauthroized: string = "Você não está autorizado a acessar esta página";
+
     // Regex
     public static nameRegex: string = "[a-zA-Z0-9 ]*";
     public static emailRegex: string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
