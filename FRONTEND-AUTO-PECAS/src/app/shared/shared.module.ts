52958f7b168d53e22b9ae21d3cffbd3055caf7bb
Lenildo <lourencolenildo@gmail.com>
2023-10-01 21:32:22 -0300

    Navbar e formulário alterar senha

--- FRONTEND-AUTO-PECAS/src/app/shared/shared.module.ts
+++ FRONTEND-AUTO-PECAS/src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { AccordionAnchorDirective, AccordionLinkDirective, AccordionDirective } from './accordion';
+import { MenuItems } from './menu-items';
 
 
 @NgModule({
@@ -13,7 +14,7 @@ import { AccordionAnchorDirective, AccordionLinkDirective, AccordionDirective } 
     AccordionAnchorDirective,
     AccordionLinkDirective,
     AccordionDirective
-   ],
-  providers: [ ]
+  ],
+  providers: [MenuItems]
 })
 export class SharedModule { }
